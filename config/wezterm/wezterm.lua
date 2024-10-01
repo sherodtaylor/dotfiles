e57f4c3cfbffbcb2a43f51541dc854fa7d4b5432
Sherod Taylor <staylor279@bloomberg.net>
2024-10-01 12:04:40 -0400

    fix wezterm lua

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = "AdventureTime"
 
-config.font = wezterm.font("Fira Code Nerd Font Mono")
+config.font = wezterm.font("FiraCode Nerd Font Mono")
 
 -- and finally, return the configuration to wezterm
 return config
