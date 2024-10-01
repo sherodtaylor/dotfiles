484ca2192efc3f429a8e8ba2820f81a67af7a212
Sherod Taylor <staylor279@bloomberg.net>
2024-10-01 11:08:33 -0400

    cleanup lazyzim

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -1,4 +1,12 @@
 return {
-  { "junegunn/fzf", dir = "~/.fzf", build = "./install --all" },
-  { "junegunn/fzf.vim" },
+  { "junegunn/fzf", build = "./install --bin" },
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup({})
+    end,
+  },
 }
