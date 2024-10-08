872578e8f28ad9aafa984b669342de7026a09a54
Sherod Taylor <staylor279@bloomberg.net>
2024-10-08 14:29:44 -0400

    fix slow git icon for fzf-lua :

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -6,7 +6,11 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       -- calling `setup` is optional for customization
-      require("fzf-lua").setup({})
-    end,
-  },
+      require("fzf-lua").setup({
+         defaults = {
+          git_icons = false
+        }
+      })
+    end
+  }
 }
